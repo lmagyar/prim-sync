471b96502424d757e991ba6e80cbe7090712f868
Laszlo Magyar <lmagyar1973@gmail.com>
2024-07-23 23:29:02 +0200

    support git-bash on win

--- prim-sync.sh
+++ prim-sync.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-${0%/*}/.venv/bin/python ${0%/*}/prim-sync.py "$@"
+if [[ "${OSTYPE:-}" == "cygwin" || "${OSTYPE:-}" == "msys" ]] ; then
+    venv_python_path="Scripts/python.exe"
+else
+    venv_python_path="bin/python"
+fi
+${0%/*}/.venv/${venv_python_path} ${0%/*}/prim-sync.py "$@"
